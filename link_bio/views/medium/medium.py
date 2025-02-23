ad3dc2744b260420d0d4e1f7952aaf5f7d9cdbb8
steph <stephaniemgonzalez10@gmail.com>
2025-02-23 12:41:15 -0400

    barra_changes

--- link_bio/views/medium/medium.py
+++ link_bio/views/medium/medium.py
@@ -1,5 +1,5 @@
 import reflex as rx
-from link_bio.components.link_icon import link_icon
+#from link_bio.components.link_icon import link_icon
 from link_bio.styles.styles import Size as Size
 from link_bio.styles.colors import TextColor as TextColor
 from link_bio.styles.colors import Color as Color
@@ -7,45 +7,31 @@ import link_bio.styles.styles as styles
 
 def medium() -> rx.Component:
     return rx.hstack(
-        rx.text("3 Years Experience", style=styles.medium_container),
-        rx.progress(
-            value=75,  # Porcentaje de experiencia
-            style={
-                "background": "linear-gradient(to right, #8e44ad, #9b59b6)",
-                "height": "20px",
-                "border-radius": "10px",
-                "width": "100%"
-            },
-            label="75%"
-        ),
-        rx.text("5 Projects Completed", style=styles.medium_container),
-        rx.progress(
-            value=100,  # Porcentaje de proyectos completados
-            style={
-                "background": "linear-gradient(to right, #8e44ad, #9b59b6)",
-                "height": "20px",
-                "border-radius": "10px",
-                "width": "100%"
-            },
-            label="100%"
-        ),
-        rx.box(
-            rx.hstack(
-                link_icon(
-                    "https://github.com/michiGonz",
-                    "/icons/github.svg"
-                ),
-                link_icon(
-                    "https://linkedin.com/in/stephanie-gonzález-87303a1b6",
-                    "/icons/linkedin.svg"
-                ),
-                link_icon(
-                    "https://x.com/stephdev",
-                    "/icons/twitter.svg"
-                ),
-                style={"gap": "20px"}  # Añade espacio entre los iconos
+        rx.vstack(
+            rx.text("3 Years Experience", style=styles.medium_container),
+            rx.progress(
+                value=75,  # Porcentaje de experiencia
+                style={
+                    "background": "linear-gradient(to right, #8e44ad, #9b59b6)",
+                    "height": "20px",
+                    "border-radius": "10px",
+                    "width": "100%"
+                },
+                label="75%"
             ),
-            style={"display": "flex", "justify-content": "flex-end", "width": "100%", "padding" : "10px"}  # Alinea los iconos a la derecha
         ),
-        style=styles.medium_container_style
+        rx.vstack(
+            rx.text("5 Projects Completed", style=styles.medium_container),
+            rx.progress(
+                value=100,  # Porcentaje de proyectos completados
+                style={
+                    "background": "linear-gradient(to right, #8e44ad, #9b59b6)",
+                    "height": "20px",
+                    "border-radius": "10px",
+                    "width": "100%"
+                },
+                label="100%"
+            ),
+        ),
+        style={"gap": "20px"}  # Añade espacio entre las barras de progreso
     )
